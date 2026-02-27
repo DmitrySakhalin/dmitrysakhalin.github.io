467fd3b73da682786f7b6a89b17da660d374d988
Dmitry Pack <dmitrypack0@gmail.com>
2026-02-27 22:04:10 +0300

    Update year 2025→2026 in footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("© 2025 Dmitry Pack — All rights reserved.")}
+          {emoji("© 2026 Dmitry Pack — All rights reserved.")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}></p>
       </div>
